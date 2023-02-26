25bf78bdb35c98559eb6a01b3ab2b0c3ef48c67f
Szzx123 <55950446+Szzx123@users.noreply.github.com>
2023-02-26 12:24:06 +0100

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xp92rd14rfxz^=s86)29371w_y05q3ju$d-eh9s@a77&45aq#5"
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
